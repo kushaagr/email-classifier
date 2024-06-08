e677bbd0ffecadaf86b0a14e127f105916147cec
Kushagra Mehrotra <68564934+kushaagr@users.noreply.github.com>
2024-06-08 08:48:12 +0530

    Fix key prop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -71,9 +71,9 @@ async function EmailsList(props: { className?: string }) {
       {/* <EmailItem email={emails[0]} label="General"/> */}
       {labelled
         ? emails.map((email, index) => (
-            <EmailItem key={`labelled-email-${index}`} email={email} label={labels[index]} />
+            <EmailItem key={`${email.id}`} email={email} label={labels[index]} />
           ))
-        : emails.map((email) => <EmailItem key={`unlabelled-email-${index}`} email={email} />)}
+        : emails.map((email) => <EmailItem key={`${email.id}`} email={email} />)}
     </div>
   );
 }
